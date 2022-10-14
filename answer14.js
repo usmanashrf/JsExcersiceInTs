7addcb9f682aa1dedce0ffe87379de4ef96cfad1
usmanashrf <45431758+usmanashrf@users.noreply.github.com>
2022-10-14 21:17:47 +0500

    Solve 14 question

---
+++ answer14.js
@@ -0,0 +1,5 @@
+"use strict";
+let guests = ['Ali', 'Mujtaba', 'Umer', 'khan'];
+for (var i = 0; i < guests.length; i++) {
+    console.log(`${guests[i]} I would like to invite you on my birthday`);
+}
---
+++ answer14.ts
@@ -0,0 +1,5 @@
+let guests:string[]=['Ali','Mujtaba','Umer','khan'];
+
+for(var i=0; i< guests.length; i++){
+    console.log(`${guests[i]} I would like to invite you on my birthday`);
+}
