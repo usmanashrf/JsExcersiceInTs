da5433d8ae97bb9741bab256ab91b9c29c04cfe4
usmanashrf <45431758+usmanashrf@users.noreply.github.com>
2022-10-14 21:09:12 +0500

    Solve 10 question

---
+++ answer10.js
@@ -0,0 +1,13 @@
+"use strict";
+let my_Name = "M Usman Ashraf";
+const now = new Date();
+// here we are getting today's date from date object
+// let dd = today.getDate().toString().padStart(2, '0');
+// var mm = String(today.getMonth() + 1).padStart(2, '0'); 
+// var yyyy = today.getFullYear();
+// today = mm + '/' + dd + '/' + yyyy;
+/*here we
+are showing data
+ in console*/
+console.log(`My name is ${my_Name}`);
+console.log(`today's date is : ${now}`);
---
+++ answer10.ts
@@ -0,0 +1,10 @@
+let my_Name :string = "M Usman Ashraf";
+
+// here we are getting today's date from date object
+const now = new Date();
+
+/*here we 
+are showing data
+ in console*/
+console.log(`My name is ${my_Name}`);
+console.log(`today's date is : ${now}`);
