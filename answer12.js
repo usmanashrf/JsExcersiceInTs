62ec3f7409a77656065cd68e1c5a0d00b4ef2d19
usmanashrf <45431758+usmanashrf@users.noreply.github.com>
2022-10-14 21:14:37 +0500

    Solve 12 question

---
+++ answer12.js
@@ -0,0 +1,5 @@
+"use strict";
+let firend_names = ['Ali', 'Mujtaba', 'Umer', 'khan'];
+for (var i = 0; i < firend_names.length; i++) {
+    console.log(`Hello my Friend ${firend_names[i]}, Hope you're doing good`);
+}
---
+++ answer12.ts
@@ -0,0 +1,5 @@
+let firend_names : string[] =['Ali','Mujtaba','Umer','khan'];
+
+for(var i=0; i< firend_names.length; i++){
+    console.log(`Hello my Friend ${firend_names[i]}, Hope you're doing good`);
+}
