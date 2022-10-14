67a281dc35b87fa10bba1544f93599afcc97f7da
usmanashrf <45431758+usmanashrf@users.noreply.github.com>
2022-10-14 20:51:35 +0500

    Solve 5 question

---
+++ answer5.js
@@ -0,0 +1,4 @@
+"use strict";
+let famous_person = "Muhammad Ali Jinnah";
+let mesaage = "“I do not believe in taking the right decision, I take a decision and make it right.”";
+console.log(`${famous_person} once said,${mesaage} `);
---
+++ answer5.ts
@@ -0,0 +1,5 @@
+
+let famous_person:string = "Muhammad Ali Jinnah";
+
+let mesaage: string = "“I do not believe in taking the right decision, I take a decision and make it right.”";
+console.log(`${famous_person} once said,${mesaage} `);
