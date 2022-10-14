778c01ebef3ea737413b518956b78988448a7bd6
usmanashrf <45431758+usmanashrf@users.noreply.github.com>
2022-10-14 20:49:17 +0500

    Solve first 4 questions

---
+++ answer4.js
@@ -0,0 +1,3 @@
+"use strict";
+let authorName = "Muhammad Ali Jinnah";
+console.log(`${authorName} once said, “I do not believe in taking the right decision, I take a decision and make it right.”`);
---
+++ answer4.ts
@@ -0,0 +1,5 @@
+
+let authorName = "Muhammad Ali Jinnah";
+
+
+console.log(`${authorName} once said, “I do not believe in taking the right decision, I take a decision and make it right.”`);
