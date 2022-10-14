242f1c16d5819434bd4941dd7f4b05b700285c76
usmanashrf <45431758+usmanashrf@users.noreply.github.com>
2022-10-14 21:56:11 +0500

    Solve 23 question

---
+++ answer23.js
@@ -0,0 +1,12 @@
+"use strict";
+let AfridiNumber = 10;
+let Score = "50";
+console.log(AfridiNumber == 10);
+console.log(AfridiNumber === 10);
+console.log(AfridiNumber == '10');
+console.log(AfridiNumber == 20 - 10);
+console.log(AfridiNumber === 5 * 2);
+console.log(AfridiNumber === '10');
+console.log(AfridiNumber == 20 + 1);
+console.log(Score === 60 - 10);
+console.log(Score == AfridiNumber);
---
+++ answer23.ts
@@ -0,0 +1,13 @@
+let AfridiNumber :any = 10;
+let Score :any="50";
+
+console.log(AfridiNumber ==10);
+console.log(AfridiNumber ===10);
+console.log(AfridiNumber =='10');
+console.log(AfridiNumber ==20-10);
+console.log(AfridiNumber ===5*2);
+
+console.log(AfridiNumber ==='10');
+console.log(AfridiNumber ==20+1);
+console.log(Score === 60-10);
+console.log(Score == AfridiNumber);
