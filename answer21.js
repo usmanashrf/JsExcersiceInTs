550bea7bb2bf1a159aa6a93629ef4dfad1253089
usmanashrf <45431758+usmanashrf@users.noreply.github.com>
2022-10-14 21:46:01 +0500

    Solve 21 question

---
+++ answer21.js
@@ -0,0 +1,7 @@
+"use strict";
+let employee = {
+    id: 1005,
+    name: "Ali",
+    Salary: "200k"
+};
+console.log(employee);
---
+++ answer21.ts
@@ -0,0 +1,16 @@
+interface employee {
+    id:number
+    name: string;
+    Salary: string;
+  }
+   
+
+
+let employee  : employee ={
+    id:1005,
+    name:"Ali",
+    Salary:"200k"
+};
+
+
+console.log(employee);
