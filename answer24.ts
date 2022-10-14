be1f34e089116b2eaac010af5f5669d1e08320ea
usmanashrf <45431758+usmanashrf@users.noreply.github.com>
2022-10-14 22:07:24 +0500

    Solve 24 question

---
+++ answer24.ts
@@ -0,0 +1,25 @@
+let today : any="sat"
+let todayDate : any=1;
+let color = "White";
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+
+
+console.log(todayDate ===1);
+console.log(todayDate ==='1');
+
+console.log(today === today.toLowerCase());
+
+console.log(today >2);
+
+console.log(today <2);
+console.log(todayDate =='1');
+
+console.log(today <2|| todayDate =='1');
+console.log(today <2|| todayDate ==='1');
+
+console.log(today=="sat" && todayDate =='1');
+console.log(today =="sat" && todayDate ==='1');
+
+
+console.log(fruits.includes("Mango"));
+console.log(fruits.includes("grapes"));
