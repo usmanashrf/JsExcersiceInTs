a2b1802e7f95b3154a98f87c61bd5479635e9cb5
usmanashrf <45431758+usmanashrf@users.noreply.github.com>
2022-10-14 21:37:11 +0500

    Solve 20 question

---
+++ answer20.js
@@ -0,0 +1,4 @@
+"use strict";
+let cities = ["ISB", "RWP", "LHR", "KCHI"];
+console.log("List of following cities");
+console.log(cities);
---
+++ answer20.ts
@@ -0,0 +1,4 @@
+let cities: string[]=["ISB","RWP","LHR","KCHI"];
+
+console.log("List of following cities");
+console.log(cities);
