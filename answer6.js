de0991fa2f99baccf5cf296dc3bf4e459ee978a2
usmanashrf <45431758+usmanashrf@users.noreply.github.com>
2022-10-14 20:54:31 +0500

    Solve 6 question

---
+++ answer6.js
@@ -0,0 +1,5 @@
+"use strict";
+let myName = "Muhammad Usman";
+console.log(`\t ${myName}`);
+console.log(`nextline: \n${myName}`);
+console.log(`${myName} \t.`);
---
+++ answer6.ts
@@ -0,0 +1,5 @@
+let myName: string= "Muhammad Usman";
+
+console.log(`\t ${myName}`);
+console.log(`nextline: \n${myName}`);
+console.log(`${myName} \t.`);
