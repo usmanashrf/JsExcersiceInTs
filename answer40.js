193a827a0ae1231005073e773238e83c6afac8f4
usmanashrf <45431758+usmanashrf@users.noreply.github.com>
2022-10-17 21:10:18 +0500

    add answer 40

---
+++ answer40.js
@@ -0,0 +1,18 @@
+"use strict";
+function make_album(artistName, albumTitle, trackNumbers) {
+    let obj = {
+        artistName: { artistName },
+        albumTitle: { albumTitle },
+        trackNumbers: { trackNumbers }
+    };
+    if (trackNumbers != undefined) {
+        obj.trackNumbers = { trackNumbers };
+    }
+    return obj;
+}
+var firstObj = make_album("Atif", "Dori");
+console.log(firstObj);
+var secObj = make_album("AliZafar", "Chano", 5);
+console.log(secObj);
+var thirdObj = make_album("Mustafa Zahid", "Chano", 2);
+console.log(thirdObj);
