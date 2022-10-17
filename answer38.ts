825d7457edf9f8a6ad2681c15e6bda3314ddd9a4
usmanashrf <45431758+usmanashrf@users.noreply.github.com>
2022-10-17 21:05:34 +0500

    add answer 38

---
+++ answer38.ts
@@ -0,0 +1,7 @@
+function describe_city(city:string, country="Pakistan"){
+    console.log(`${city} is in ${country}`);
+    }
+    
+    describe_city("Karachi");
+    describe_city("Lahore");
+    describe_city('London',"UK");
