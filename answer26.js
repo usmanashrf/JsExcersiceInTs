ee695273f9216c43d1ed25df4551d71bfa35d032
usmanashrf <45431758+usmanashrf@users.noreply.github.com>
2022-10-17 20:45:13 +0500

    add answer 26

---
+++ answer26.js
@@ -0,0 +1,15 @@
+"use strict";
+let alienclr = "green";
+if (alienclr === "green") {
+    console.log("player just earned 5 points.");
+}
+else {
+    console.log("player just earned 10 points.");
+}
+let alienclor = "yellow";
+if (alienclor === "green") {
+    console.log("player just earned 5 points.");
+}
+else {
+    console.log("player just earned 10 points.");
+}
---
+++ answer26.ts
@@ -0,0 +1,17 @@
+let alienclr ="green"
+
+if(alienclr ==="green"){
+    console.log("player just earned 5 points.")
+}
+else{
+    console.log("player just earned 10 points.")
+}
+
+let alienclor ="yellow"
+
+if(alienclor ==="green"){
+    console.log("player just earned 5 points.")
+}
+else{
+    console.log("player just earned 10 points.")
+}
