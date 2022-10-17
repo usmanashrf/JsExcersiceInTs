fd1961f2ff2288b4b24e40d2edea0eaa3be79850
usmanashrf <45431758+usmanashrf@users.noreply.github.com>
2022-10-17 20:50:14 +0500

    add answer 28

---
+++ answer28.js
@@ -0,0 +1,20 @@
+"use strict";
+let age = 15;
+if (age < 2) {
+    console.log("That person is baby");
+}
+else if (age >= 2 && age < 4) {
+    console.log("That person is baby");
+}
+else if (age >= 4 && age < 13) {
+    console.log("That the person is a kid");
+}
+else if (age >= 13 && age < 20) {
+    console.log("That person is a teenager");
+}
+else if (age >= 20 && age < 65) {
+    console.log("That person is an adult");
+}
+else if (age > 65) {
+    console.log("That person is an older");
+}
