89c6aadc7f809c646317cc060e6adf1168d87c2d
usmanashrf <45431758+usmanashrf@users.noreply.github.com>
2022-10-17 21:23:01 +0500

    add answer 44

---
+++ answer44.js
@@ -0,0 +1,7 @@
+"use strict";
+function Sandwitch(...items) {
+    console.log(items);
+}
+Sandwitch("bread", 'cheez', 'patato', 'egg');
+Sandwitch("menu", 'ketchup');
+Sandwitch("Onion", 'cheez', 'egg');
---
+++ answer44.ts
@@ -0,0 +1,10 @@
+function Sandwitch(...items:string[]){
+    console.log(items);
+}
+
+
+Sandwitch("bread", 'cheez','patato','egg');
+
+Sandwitch("menu", 'ketchup');
+
+Sandwitch("Onion", 'cheez','egg');
