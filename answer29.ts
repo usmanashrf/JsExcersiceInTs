e06b2731f66b827966a727faebc3cbf911af4af1
usmanashrf <45431758+usmanashrf@users.noreply.github.com>
2022-10-17 20:51:30 +0500

    add answer 29

---
+++ answer29.ts
@@ -0,0 +1,21 @@
+let favorite_fruits = ["Apple", "banana","pear"];
+
+
+if(favorite_fruits.includes("Apple")){
+    console.log(`You really like ${favorite_fruits[0]}`);
+}
+
+if(favorite_fruits[1] =='banana'){
+    console.log(`You really like ${favorite_fruits[1]}`);
+}
+if(favorite_fruits[1] !== favorite_fruits[2] ){
+    console.log(`You really like ${favorite_fruits[1]}`);
+}
+if(favorite_fruits.includes("Apple") && favorite_fruits.includes("pear"))
+{
+    console.log(`You really like ${favorite_fruits[0]} & ${favorite_fruits[2]}`);
+}
+if(favorite_fruits.includes("Apple") || favorite_fruits.includes("mango"))
+{
+    console.log(`You really like ${favorite_fruits[0]}`);
+}
