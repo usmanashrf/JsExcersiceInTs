a8de17abe55a405153f74739545f55900eeb052b
usmanashrf <45431758+usmanashrf@users.noreply.github.com>
2022-10-17 21:06:37 +0500

    add answer 39

---
+++ answer39.js
@@ -0,0 +1,10 @@
+"use strict";
+function city_country(city, country) {
+    return `${city}, ${country}`;
+}
+var firstcall = city_country("Islamabad", "Pakistan");
+console.log(firstcall);
+var secondcall = city_country("Karachi", "Pakistan");
+console.log(secondcall);
+var thirscall = city_country("Peshawar", "Pakistan");
+console.log(thirscall);
---
+++ answer39.ts
@@ -0,0 +1,10 @@
+function city_country(city:string,country:string){
+
+    return`${city}, ${country}`;
+}
+var firstcall = city_country("Islamabad","Pakistan");
+console.log(firstcall);
+var secondcall =city_country("Karachi","Pakistan");
+console.log(secondcall);
+var thirscall = city_country("Peshawar","Pakistan");
+console.log(thirscall);
