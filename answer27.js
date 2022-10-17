2113f008474290145c993b58838ad306ad5ed498
usmanashrf <45431758+usmanashrf@users.noreply.github.com>
2022-10-17 20:49:03 +0500

    add answer 27

---
+++ answer27.js
@@ -0,0 +1,31 @@
+"use strict";
+let alien_color1 = "green";
+if (alien_color1 === "green") {
+    console.log("player just earned 5 points.");
+}
+else if (alien_color1 === "yellow") {
+    console.log("player just earned 10 points.");
+}
+else {
+    console.log("player just earned 15 points.");
+}
+let alienclr2 = "red";
+if (alienclr2 === "green") {
+    console.log("player just earned 5 points.");
+}
+else if (alienclr2 === "Yellow") {
+    console.log("player just earned 10 points.");
+}
+else {
+    console.log("player just earned 15 points.");
+}
+let aliencolor3 = "yellow";
+if (aliencolor3 === "green") {
+    console.log("player just earned 5 points.");
+}
+else if (aliencolor3 === "yellow") {
+    console.log("player just earned 10 points.");
+}
+else {
+    console.log("player just earned 15 points.");
+}
