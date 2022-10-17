2f6619edec086df31d8ef64b7f50ea3ed6ea468c
usmanashrf <45431758+usmanashrf@users.noreply.github.com>
2022-10-17 21:02:00 +0500

    add answer 36

---
+++ answer36.js
@@ -0,0 +1,6 @@
+"use strict";
+function make_shirt(sizeOfShirt, message) {
+    console.log(`size of your shirt is: ${sizeOfShirt}`);
+    console.log(`message: ${message}`);
+}
+make_shirt(15, "Hello world");
---
+++ answer36.ts
@@ -0,0 +1,7 @@
+function make_shirt(sizeOfShirt:number,message:string){
+
+    console.log(`size of your shirt is: ${sizeOfShirt}`);
+    console.log(`message: ${message}`);
+    }
+    
+    make_shirt(15,"Hello world");
