7be6c42f8a9ad4471f7c6c92b81fff58cdb53bfa
usmanashrf <45431758+usmanashrf@users.noreply.github.com>
2022-10-17 21:03:43 +0500

    add answer 37

---
+++ answer37.ts
@@ -0,0 +1,11 @@
+function make_shirts(sizeOfShirt ="Large",message="I love JavaScript"){
+
+    console.log(`size of your shirt is: ${sizeOfShirt}`);
+    console.log(`message: ${message}`);
+    }
+    
+    make_shirts();
+    make_shirts("medium");
+
+    make_shirts("extra large","Hello world");
+    
