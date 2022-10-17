6252597ca6ef1c7eca3f699c343818be3a215dcc
usmanashrf <45431758+usmanashrf@users.noreply.github.com>
2022-10-17 20:59:00 +0500

    add answer 34

---
+++ answer34.js
@@ -0,0 +1,10 @@
+"use strict";
+let pizzas = ["Veggie Pizza", "Pepperoni Pizza", "BBQ Chicken Pizza"];
+for (var i = 0; i < pizzas.length; i++) {
+    console.log(pizzas[i]);
+}
+for (var i = 0; i < pizzas.length; i++) {
+    console.log(` I like  ${pizzas[i]}`);
+}
+console.log(`I love pizza so much, 
+it's one of the best in fast food with full of  nutritions. `);
