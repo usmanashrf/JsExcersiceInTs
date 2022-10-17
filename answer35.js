d729f5a87da8a8eec70def4b8ac053f91800545a
usmanashrf <45431758+usmanashrf@users.noreply.github.com>
2022-10-17 21:00:22 +0500

    add answer 35

---
+++ answer35.js
@@ -0,0 +1,11 @@
+"use strict";
+let animals = ["dog", 'cat', "parrort"];
+for (var i = 0; i < animals.length; i++) {
+    console.log(animals[i]);
+}
+for (var i = 0; i < animals.length; i++) {
+    console.log(`${animals[i]} is a good pet`);
+}
+console.log(`${animals[0]} is a loyal pet`);
+console.log(`${animals[1]} is a funny pet`);
+console.log(`${animals[2]} is a beautyful pet`);
