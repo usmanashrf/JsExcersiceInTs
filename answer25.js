1696625c3d3018ce3797f64577efea515d27a197
usmanashrf <45431758+usmanashrf@users.noreply.github.com>
2022-10-17 20:42:40 +0500

    add answer 25

---
+++ answer25.js
@@ -0,0 +1,9 @@
+"use strict";
+let alien_color = "green";
+if (alien_color === "green") {
+    console.log("player just earned 5 points.");
+}
+let aliencolor = "yellow";
+if (aliencolor === "green") {
+    console.log("player just earned 5 points.");
+}
---
+++ answer25.ts
@@ -0,0 +1,12 @@
+let alien_color ="green"
+
+if(alien_color ==="green"){
+    console.log("player just earned 5 points.")
+}
+
+
+let aliencolor ="yellow"
+
+if(aliencolor ==="green"){
+    console.log("player just earned 5 points.")
+}
